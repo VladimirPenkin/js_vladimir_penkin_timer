ad649079d2d0ce100a4a429ba1b96d78b3b5d5c4
Vladimir Penkin <Vladimir.Penkin@kyivstar.ua>
2020-03-11 16:17:45 +0200

    добавлена остановка счётчика

--- main.js
+++ main.js
@@ -9,51 +9,15 @@ function startTime () {
 			time.innerHTML = i++;
 		} else {
 			stopTimer(myTime);
-		}
-	}
-}
+		};
+	};
+};
 
 function stopTime () {
 	clearTimeout(myTime);
-}
+};
 
 function clearTime () {
 	clearInterval(myTime);
 	time.innerHTML = 1;
-}
-
-// let count = 1;
-// let count = 30;
-// var startTimer;
-
-// function startMyTimer() {
-//     stopTimer();
-//     startTimer= setInterval(function(){
-//         // if (count < 31) {
-//         if (count > 0) {
-//             document.getElementById('time').innerHTML = count;
-//             // count++
-//             count--
-//         } else {
-//             stopTimer(startTimer);
-//         }
-//     }, 1000);
-// }
-
-// function stopTimer() {
-//     clearInterval(startTimer);
-// }
-
-// function reset() {
-//     // count = 1;
-//     count = 30;
-//     document.getElementById('time').innerHTML = count;
-// }
-
-// startMyTimer();
-
-
-
-
-
-
+};
